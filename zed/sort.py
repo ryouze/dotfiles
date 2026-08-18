c29d59cb84d18628fdb1e0cec12e41f1fae6d316
ryouze <mirai@ryouze.net>
2026-08-18 21:59:42 +0200

    Create `zed/`.

---
+++ zed/sort.py
@@ -0,0 +1,19 @@
+# /// script
+# dependencies = ["json5"]
+# ///
+
+"""
+Usage: uv run zed/sort.py
+"""
+
+import json
+from pathlib import Path
+
+import json5
+
+path = Path(__file__).with_name("settings.json")
+
+data = json5.loads(path.read_text())
+out: str = json.dumps(data, indent=2, sort_keys=True) + "\n"
+
+path.write_text(out)
